cfa1a58a6dde0117ac0364cb5f00423d8f34165b
garniik <antoine.arnoultfr@gmail.com>
2025-09-12 10:22:35 +0200

    fix(map):island and water spritesheets

---
+++ assets/island/island_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="island_spritesheet" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="png/island_spritesheet.png" width="128" height="160"/>
+</tileset>
