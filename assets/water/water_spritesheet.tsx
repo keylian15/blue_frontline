4067bd3fa7cff992e3639b1060f95600edc7918b
keylian15 <keylian59555@gmail.com>
2025-09-19 11:11:19 +0200

    fix(map): Changement avec la map correct

--- assets/water/water_spritesheet.tsx
+++ assets/water/water_spritesheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="water_spritesheet" tilewidth="32" tileheight="32" tilecount="15" columns="3">
- <image source="png/water_spritesheet.png" width="96" height="160"/>
+<tileset version="1.10" tiledversion="1.11.2" name="water_spritesheet" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="png/water_spritesheet.png" width="512" height="512"/>
 </tileset>
