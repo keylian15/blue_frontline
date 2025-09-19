411db4334bb484398dd6d3a7266bacc41ca5bfba
Keylian Turbé <keylian59555@gmail.com>
2025-09-19 17:07:23 +0200

    REBASE Propre (#32)
    * Entités + tir (#15)
    
    * Commit Entités/Unités + déplacements + modifs
    
    * fix(map): Rajout de la map correct
    
    * fix(import): time
    
    * fix(quantique): changement en clic droit
    
    * Regroupement unités + changement variables dans Global
    
    * fix(map): Changement avec la map correct
    
    * Ajout zoom/dézoom Camera
    
    * Suppr inutile + Réorganisation
    
    ---------
    
    Co-authored-by: keylian15 <keylian59555@gmail.com>
    
    * fix(main): correction touches
    
    * feat(texture):eau
    
    * fix(opti): mouvement de la cam
    
    ---------
    
    Co-authored-by: tcmy6526512 <169056136+tcmy6526512@users.noreply.github.com>
    Co-authored-by: SIAME <siame.romain.scw@gmail.com>
    Co-authored-by: garniik <antoine.arnoultfr@gmail.com>

--- Class/InputManager.py
+++ Class/InputManager.py
@@ -1,5 +1,5 @@
 import pygame
-
+import time
 class InputManager:
     """Gestionnaire des entrées continues pour le jeu."""
     
@@ -11,43 +11,57 @@ class InputManager:
     
     def handle_continuous_input(self):
         """Gère les entrées continues (touches maintenues)."""
-        if not self.game.show_unit_popup:
-            self._handle_camera_movement()
-            self._handle_hud_toggle()
-            self._handle_zoom()
-    
-    def _handle_camera_movement(self):
-        """Gère le déplacement de la caméra avec les touches directionnelles."""
         pressed = pygame.key.get_pressed()
         
+        # if not self.game.show_unit_popup:
+        self._handle_camera_movement(pressed)
+        self._handle_hud_toggle(pressed)
+        self._handle_zoom(pressed)
+        self._handle_unit_popup(pressed)
+        self._handle_unit_popup_navigation(pressed)
+    
+    def _handle_camera_movement(self, pressed):
+        """Gère le déplacement de la caméra avec les touches directionnelles."""
+        
         dx, dy = 0, 0
-        if pressed[pygame.K_UP]:  # Haut
+        if pressed[pygame.K_z]:  # Haut
             dy -= self.game.camera.camera_move
-        if pressed[pygame.K_DOWN]:  # Bas
+        if pressed[pygame.K_s]:  # Bas
             dy += self.game.camera.camera_move
-        if pressed[pygame.K_LEFT]:  # Gauche 
+        if pressed[pygame.K_q]:  # Gauche 
             dx -= self.game.camera.camera_move
-        if pressed[pygame.K_RIGHT]:  # Droite
+        if pressed[pygame.K_d]:  # Droite
             dx += self.game.camera.camera_move
         
         # Déplacer la caméra seulement s'il y a un déplacement
         if dx or dy:  
             self.game.camera.move(dx, dy)
     
-    def _handle_hud_toggle(self):
+    def _handle_hud_toggle(self, pressed):
         """Gère l'affichage/masquage du HUD avec cooldown non-bloquant."""
-        pressed = pygame.key.get_pressed()
         current_time = pygame.time.get_ticks()
         
-        if pressed[pygame.K_h] and current_time - self.last_hud_toggle_time > self.hud_toggle_cooldown:
-            self.game.hud.switch()
-            self.last_hud_toggle_time = current_time
+        if pressed[pygame.K_h] :
+            if current_time - self.last_hud_toggle_time > self.hud_toggle_cooldown:
+                self.game.hud.switch()
+                self.last_hud_toggle_time = current_time
     
-    def _handle_zoom(self):
+    def _handle_zoom(self, pressed):
         """Gère le zoom avec les touches P et M."""
-        pressed = pygame.key.get_pressed()
-        
         if pressed[pygame.K_p]:  # Touche P pour dézoomer
             self.game.camera.zoom_out()
         if pressed[pygame.K_m]:  # Touche M pour zoomer
             self.game.camera.zoom_in()
+            
+    def _handle_unit_popup(self, pressed):
+        if pressed[pygame.K_j]:
+            self.game.hud.toggle_popup_team()
+            time.sleep(0.2)
+            
+    def _handle_unit_popup_navigation(self, pressed):
+        if pressed[pygame.K_LEFT]:
+            self.game.hud.popup_selection = (self.game.hud.popup_selection - 1) % len(self.game.hud.unit_names)
+            time.sleep(0.1)
+        if pressed[pygame.K_RIGHT]:
+            self.game.hud.popup_selection = (self.game.hud.popup_selection + 1) % len(self.game.hud.unit_names)
+            time.sleep(0.1)
