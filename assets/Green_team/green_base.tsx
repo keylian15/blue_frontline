676d0b063e80e9b39821cd3e0ca6970a2a857b71
keylian15 <keylian59555@gmail.com>
2025-09-22 16:18:43 +0200

    feat(map): Implémentation des zones de spawn

---
+++ assets/Green_team/green_base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="green_base" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="png/green_base.png" width="224" height="224"/>
+</tileset>
