e56cc1c12438edb0c84a2ffb4172b7a61b19a2b7
keylian15 <keylian59555@gmail.com>
2025-09-30 23:35:15 +0200

    Docs(Piece): Docs + Typage

--- blue_frontline/Class/Piece.py
+++ blue_frontline/Class/Piece.py
@@ -1,16 +1,6 @@
-import pygame
-
 class Piece:
+    """Classe pour gérer les pièces."""
+
     def __init__(self):
+        """Initialise les pièces."""
         self.count = 0
-        
-    
-    def add_one(self):
-        self.count += 1
-        
-    def minus_one(self):
-        if self.count <= 0:
-            return
-        else:
-            self.count -= 1
-    
