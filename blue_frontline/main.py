2a2f4fa990e22c2a4c54756ea18dcff23ff9e6a3
Thomas <thomaschampy00@gmail.com>
2025-10-24 10:45:32 +0200

    modif main.py

--- blue_frontline/main.py
+++ blue_frontline/main.py
@@ -3,14 +3,6 @@ from Class.menu import *
 import Global
 
 if __name__ == "__main__":
-    # supprimer tout les __pycache__ avant de lancer le jeu de manière récursive (pour sous dossiers)
-    import shutil
-    import os
-    for root, dirs, files in os.walk("."):
-        for dir_name in dirs:
-            if dir_name == "__pycache__":
-                shutil.rmtree(os.path.join(root, dir_name))
-
     pygame.init()
     pygame.display.set_caption("Menu du jeu")
     
