d2dbc7bfef9b7a90309f72d0e1a7cc7604b07140
keylian15 <keylian59555@gmail.com>
2025-11-26 14:17:00 +0100

    feat(menu): ajout du menu credit

--- blue_frontline/Class/menu.py
+++ blue_frontline/Class/menu.py
@@ -27,13 +27,14 @@ class Menu:
 
         # Boutons
         start_x = BUTTON_MARGIN_LEFT
-        start_y = self.HEIGHT - (5 * BUTTON_HEIGHT + 3 * BUTTON_SPACING) - BUTTON_MARGIN_BOTTOM
+        start_y = self.HEIGHT - (3 * BUTTON_HEIGHT + 3 * BUTTON_SPACING) - BUTTON_MARGIN_BOTTOM
         self.buttons = [
             ("Jouer",   start_x, start_y, BUTTON_WIDTH, BUTTON_HEIGHT),
-            ("Tuto",  start_x, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 1, BUTTON_WIDTH, BUTTON_HEIGHT),
+            ("Succès",  start_x, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 1, BUTTON_WIDTH, BUTTON_HEIGHT),
             ("Options", start_x, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 2, BUTTON_WIDTH, BUTTON_HEIGHT),
-            ("Succès", start_x, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 3, BUTTON_WIDTH, BUTTON_HEIGHT),
-            ("Quitter", start_x, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 4, BUTTON_WIDTH, BUTTON_HEIGHT),
+            ("Tuto", start_x + BUTTON_WIDTH + BUTTON_SPACING, start_y, BUTTON_WIDTH, BUTTON_HEIGHT),
+            ("Credits", start_x + BUTTON_WIDTH + BUTTON_SPACING, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 1, BUTTON_WIDTH, BUTTON_HEIGHT),
+            ("Quitter", start_x + BUTTON_WIDTH + BUTTON_SPACING, start_y + (BUTTON_HEIGHT + BUTTON_SPACING) * 2, BUTTON_WIDTH, BUTTON_HEIGHT),
         ]
         
         
@@ -193,6 +194,10 @@ class Menu:
                             game = Game(self.screen, "tuto")
                             game.achievements_system = self.achievements_system
                             game.run()
+                        elif text == "Credits" : 
+                            from Class.CreditsMenu import CreditsMenu
+                            credits_menu = CreditsMenu(self.screen)
+                            credits_menu.run()
                             
 
                 pygame.display.flip()
