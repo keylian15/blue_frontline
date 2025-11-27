3b91f1b12bcb1bd30380ad61da5a18f6bd939358
keylian15 <keylian59555@gmail.com>
2025-11-27 16:06:02 +0100

    fix(speed): application des vitesses réduites

--- blue_frontline/src/config/game_constants.py
+++ blue_frontline/src/config/game_constants.py
@@ -5,7 +5,7 @@ FPS = 60
 TIME_STEP = 1000  # en ms => 1 seconde
 
 # Vitesses de temps disponibles
-TIME_SPEEDS = [1, 2, 4, 8, 10, 20, 0.5]
+TIME_SPEEDS = [1, 2, 4, 0.5]
 
 # === Événements Pygame ===
 PETROLE_EVENT = pygame.USEREVENT + 1
